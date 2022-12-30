f69230995034b63cb68907e67052e8829e180c32
Wii2 <The1stWii2@outlook.com>
2022-12-30 15:26:15 +0000

    Add angry note

--- src/coreLib/store-interfacing.ts
+++ src/coreLib/store-interfacing.ts
@@ -6,7 +6,7 @@ interface JSONLoose {
   [key: string]: any[] | any; //A looser definition for JSON Objects, which TypeScript can better parse.
 }
 
-export function loadStoreFile<T extends JSONValue = JSONValue>(guildID: `${number}` | "global", command?: string): T {
+export function loadStoreFile<T = JSONValue>(guildID: `${number}` | "global", command?: string): T | undefined {
   if (fs.existsSync(path.join(__CONFIGURATION__.filepaths.storageLocation, `${guildID}.json`))) {
     const store = JSON.parse(
       String(fs.readFileSync(path.join(__CONFIGURATION__.filepaths.storageLocation, `${guildID}.json`)))
@@ -15,6 +15,9 @@ export function loadStoreFile<T extends JSONValue = JSONValue>(guildID: `${numbe
     if (command === undefined) {
       return store as T;
     }
+    //This returns the object still within the command's Object. We'll need to unwrap that.
+    //TODO: Actually unwrap the top level. Object.values()[0] just crashes for some reason.
+    //But doesn't crash depending where its called???
     return store[command] as T;
   }
   throw new Error(`Storage file for "${guildID}" was requested, but no such file exists!`);
