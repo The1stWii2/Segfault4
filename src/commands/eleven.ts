b8cf3cc38e1e7308311fd60baa8167669f4d5ec7
Wii2 <The1stWii2@outlook.com>
2023-02-06 06:23:28 +0000

    Update Eleven command to expose parameters

--- src/commands/eleven.ts
+++ src/commands/eleven.ts
@@ -18,27 +18,59 @@ const Eleven: ICommand = {
   builder: new DiscordJS.SlashCommandBuilder()
     .setName("eleven")
     .setDescription("Use ElevenLabs' Speech Synthesis system.")
-    .addStringOption((option) => option.setName("input").setDescription("Text to use.").setRequired(true)),
+    .addStringOption((option) => option.setName("input").setDescription("Text to use.").setRequired(true))
+    .addNumberOption((option) =>
+      option
+        .setName("stability")
+        .setRequired(false)
+        .setMinValue(0)
+        .setMaxValue(1)
+        .setDescription("Sets the stability of the generation (default is 0.075)")
+    )
+    .addNumberOption((option) =>
+      option
+        .setName("similarity")
+        .setRequired(false)
+        .setMinValue(0)
+        .setMaxValue(1)
+        .setDescription("Sets the similarity of the generation (default is 0.8)")
+    ),
   episode: async (interaction: DiscordJS.ChatInputCommandInteraction) => {
     //This will take some time, so let's defer the reply.
     await interaction.deferReply();
 
-    const voiceRequestBody = {
-      text: interaction.options.getString("input")!,
-    };
-
-    const URL = "https://api.elevenlabs.io/v1/text-to-speech/" + __HACK_VOICE_ID_STORE["Cave Johnson"];
-
-    const response = await fetch(URL, {
+    //Set stability & similarity
+    const settingsURL = `https://api.elevenlabs.io/v1/voices/${__HACK_VOICE_ID_STORE["Cave Johnson"]}/settings/edit`;
+    const settingsResponse = await fetch(settingsURL, {
       method: "POST",
-      body: JSON.stringify(voiceRequestBody),
+      body: JSON.stringify({
+        stability: interaction.options.getNumber("stability") ?? 0.075,
+        similarity_boost: interaction.options.getNumber("similarity") ?? 0.8,
+      }),
       headers: { accept: "application/json", "Content-Type": "application/json", "xi-api-key": APIKey },
     });
 
-    const possibleFile = await response.arrayBuffer();
+    if (settingsResponse.status != 200) {
+      await interaction.editReply({
+        content: `An error occurred! Status code:\n\`\`\`\n${settingsResponse.status}\n${settingsResponse.statusText}\n\`\`\``,
+      });
+      return;
+    }
 
+    //Okay, now we can generate the voice
+
+    const voiceURL = "https://api.elevenlabs.io/v1/text-to-speech/" + __HACK_VOICE_ID_STORE["Cave Johnson"];
+
+    const voiceResponse = await fetch(voiceURL, {
+      method: "POST",
+      body: JSON.stringify({
+        text: interaction.options.getString("input")!,
+      }),
+      headers: { accept: "application/json", "Content-Type": "application/json", "xi-api-key": APIKey },
+    });
+
+    const possibleFile = await voiceResponse.arrayBuffer();
     const data = new DataView(possibleFile);
-
     //Lazy check to see if it has mp3 magic bytes
     //https://en.wikipedia.org/wiki/List_of_file_signatures
     if (
@@ -56,7 +88,7 @@ const Eleven: ICommand = {
     }
 
     await interaction.editReply({
-      content: `An error occurred! Status code:\n\`\`\`\n${response.status}\n${response.statusText}\n\`\`\``,
+      content: `An error occurred! Status code:\n\`\`\`\n${voiceResponse.status}\n${voiceResponse.statusText}\n\`\`\``,
     });
   },
 };
@@ -66,7 +98,6 @@ const Module: IModule = {
   info: { name: "Eleven", shortDescr: "Connect to Eleven" },
   init: (store) => {
     APIKey = (store as { key: string }).key;
-    console.log(APIKey);
     __COMMAND_HANDLER.addCommandGlobal(Eleven, "Eleven");
   },
 };
